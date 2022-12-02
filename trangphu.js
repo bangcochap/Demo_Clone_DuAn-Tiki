789807d2ca9c9b63ed57754ef3547a304b2dc5a6
bangcochap <huubang16042002@gmail.com>
2022-12-02 11:11:56 +0700

    Code_

---
+++ trangphu.js
@@ -0,0 +1,10 @@
+var doAn='Đồ Án môn Web cơ bản, Demo dự án Tiki';
+var fullName='Sinh viên: Nguyễn Hữu Bằng';
+var logoName='Chú ý: Ấn zô Logo để quay lại trang chủ'
+alert(doAn);
+alert(fullName);
+alert(logoName);
+function Thongbao()
+{
+    alert("Tính năng đang được cập nhật !!! ");
+}
